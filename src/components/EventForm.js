c2e7c54e552732d3b91c7e6d58d60ee41be3ab3c
里屋純平 <satoya@satoyajunpei-no-MacBook-Pro.local>
2019-08-25 20:57:39 +0900

    update

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -15,9 +15,9 @@ const EventForm = () => {
     e.preventDefault()
 
     dispatch({
-     type: CREATE_EVENT,
-     title,
-     body
+      type: CREATE_EVENT,
+      title,
+      body
     })
 
     setTitle('')
@@ -31,23 +31,23 @@ const EventForm = () => {
   }
 
   const unCreatable = title === '' || body === ''
-
+  console.log(state);
   return (
     <>
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}/>
+          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)} />
         </div>
 
         <div className="form-group">
           <label htmlFor="formEventBody">ボディー</label>
-          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}/>
+          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
         </div>
 
         <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
-        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.length === 0}>全てのイベントを削除する</button>
+        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.events.length === 0}>全てのイベントを削除する</button>
       </form>
     </>
   )
